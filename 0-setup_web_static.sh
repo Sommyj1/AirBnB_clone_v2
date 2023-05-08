f9b4495156cfdf37fa2bbabeb930ab76f9175575
Sommyj1 <joseph2blessing2015@gmail.com>
2023-05-08 19:13:46 +0000

    Prepare my web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 #A Bash script that sets up your web servers for the deployment of web_static
 
-apt-get update
-apt-get install -y nginx
+if [ ! -x "$(command -v nginx)" ]; then
+sudo apt-get update
+sudo apt-get -y install nginx
+fi
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 
 echo "
 <html>
@@ -16,31 +17,13 @@ echo "
   </body>
 </html>
 
-" /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+" | sudo tee /data/web_static/releases/test/index.html
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+sudo ln -sf '/data/web_static/releases/test/' '/data/web_static/current'
 
-printf %s "server {
-    listen 80 default_server;
 
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
+sudo sed -i 's#server_name _;#server_name _;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n#g' /etc/nginx/sites-available/default
 
-service nginx restart
+sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
+
+sudo service nginx restart
