775d4c5aba0ab7f8980c24b35e4b95b2a71273d8
Sommyj1 <joseph2blessing2015@gmail.com>
2023-06-05 22:09:49 -0700

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python3
-"""State Module for HBNB project."""
-
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models import storage_type
+from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """State class."""
+    """State class"""
+    __tablename__ = 'states'
 
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship(
+            'City',
+            backref='state',
+            cascade='delete'
+        )
+    else:
+        name = ''
 
-    @property
-    def cities(self):
-        """All cities associated with city id."""
-
-        cities_list = []
-        # from models import storage
-        # all_object = storage.all()
-        # review_list = []
-        for city in models.storage.all(City).values():
-            if city.state_id == self.id:
-                review_list.append(city)
-        return review_list
+        @property
+        def cities(self):
+            '''
+            Returns the list of City instances with state_id
+            equals the current State.id
+            State.id is the FileStorage relationship between State and City
+            '''
+            from models import storage
+            related_cities = []
+            cities = storage.all(City)
+            for city in cities.values():
+                if city.state_id == self.id:
+                    related_cities.append(city)
+            return related_cities
